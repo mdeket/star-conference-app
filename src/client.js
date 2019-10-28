a0372197d18b20ed3912817e6ca6d55f4fdfa659
Milan Deket <milandeket@gmail.com>
2019-10-28 21:38:26 +0100

    Added styling and broadcasting of events to all connected clients.

--- src/client.js
+++ src/client.js
@@ -1,54 +1,93 @@
 // const Chart = require('chart.js');
 
-const url = 'ws://localhost:8080';
+const url = 'ws://192.168.1.105:8080';
 const connection = new WebSocket(url);
 let myChart;
 
 connection.onopen = () => {
-  connection.send('hey')
+    connection.send('hey')
 };
 
 connection.onerror = (error) => {
-  console.log(`WebSocket error: ${error}`)
+    console.log(`WebSocket error: ${error}`)
 };
 
 connection.onmessage = (e) => {
-  let response = JSON.parse(e.data);
-  myChart.data.datasets[0].data[0] = response.successes;
-  myChart.data.datasets[0].data[1] = response.fails;
-  myChart.update();
+    let response = JSON.parse(e.data);
+    let success = response.successes;
+    let fails = response.fails;
+    myChart.data.datasets[0].data[0] = success;
+    myChart.data.datasets[0].data[1] = fails;
+    if(success >= fails && success > myChart.options.scales.yAxes[0].ticks.max) {
+        myChart.options.scales.yAxes[0].ticks.max = success + 5;
+    }
+
+    if(success < fails && fails > myChart.options.scales.yAxes[0].ticks.max) {
+        myChart.options.scales.yAxes[0].ticks.max = fails + 5;
+    }
+
+    myChart.update();
 };
 
 sendMessage = (data) => {
-  connection.send(data);
+    connection.send(data);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('Your document is ready!');
+    console.log('Your document is ready!');
 
-  let success = document.getElementById("succeeded");
-  success.addEventListener("click", () => sendMessage(1));
 
-  let fail = document.getElementById("failed");
-  fail.addEventListener("click", () => sendMessage(0));
+    Chart.defaults.global.defaultFontColor = 'white';
 
-  let ctx = document.getElementById('bar-chart');
-  myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ["Succeeded", "Failed"],
-      datasets: [
-        {
-          label: "Votes",
-          backgroundColor: ["#3e95cd", "#8e5ea2"],
-          data: [0, 0]
+    let success = document.getElementById("succeeded");
+    success.addEventListener("click", () => sendMessage(1));
+
+    let fail = document.getElementById("failed");
+    fail.addEventListener("click", () => sendMessage(0));
+
+    let ctx = document.getElementById('bar-chart');
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Succeeded", "Failed"],
+            datasets: [
+                {
+                    label: "Votes",
+                    backgroundColor: ["#47b9e7", "#47b9e7"],
+                    data: [0, 0]
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            aspectRatio: 1,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    // This more specific font property overrides the global property
+                    fontColor: 'white',
+                    defaultFontFamily: 'Montserrat'
+                }
+            },
+            hover: {
+                mode: 'label'
+            },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true,
+                        stepValue: 2,
+                        max: 6,
+                        min: 0
+                    }
+                }]
+            },
+            title: {
+                display: true,
+                text: 'Result'
+            }
         }
-      ]
-    },
-    options: {
-      maintainAspectRatio: false,
-      aspectRatio:1
-    }
-  });
+    });
 });
 
--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 const WebSocket = require('ws');
+const connect = require('connect');
+const serveStatic = require('serve-static');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({port: 8080});
 
 let successes = 0;
 let fails = 0;
 
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(message);
-    if(message == "1") {
-      successes += 1;
-    } else if(message == "0"){
-      fails += 1;
-    }
-    ws.send(JSON.stringify({successes, fails}));
+    ws.on('message', (message) => {
+        console.log(message);
+        if (message === "1") {
+            successes += 1;
+        } else if (message === "0") {
+            fails += 1;
+        }
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({successes, fails}));
+            }
+        });
+    });
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({successes, fails}));
+        }
+    });
+});
 
-  });
-  ws.send("Server: Connected")
+connect().use(serveStatic(__dirname)).listen(8081, function () {
+    console.log('Server running on 8080...');
 });
